1c489e1486b0759c648c3bfa4d0233526e704b62
mengchaoshen <769193938@qq.com>
2014-11-24 22:52:14 +0800

    2

---
+++ src/com/chart/action/LoginAction.java
@@ -0,0 +1,20 @@
+package com.chart.action;
+
+import android.content.Context;
+
+import com.chart.BaseAction;
+import com.lidroid.xutils.http.RequestParams;
+
+public class LoginAction extends BaseAction {
+
+	public LoginAction(Context context, String url, RequestParams params) {
+		super(context, url, params);
+	}
+
+	@Override
+	public void paramParse(String jsonResult) {
+
+		
+	}
+
+}
